127b1dd7b064826749291282ebf52b9000afd17e
Joonas Leminen <kaktus994hotmail.com>
2019-05-20 10:21:29 +0300

    Contact-list-item ja contact-list toimii

--- src/app/contact/contact-list/contact-list.component.ts
+++ src/app/contact/contact-list/contact-list.component.ts
@@ -20,4 +20,8 @@ export class ContactListComponent implements OnInit {
     console.log(this.contacts);
   }
 
+  onContactSelected(contact: Contact): void {
+    console.log(contact);
+    // alert(contact.firstName);
+  }
 }
